99e579ab7629ac70a580c04339da6aa9d5ea7751
zhanghaitao <abugtao@163.com>
2022-09-21 15:51:53 +0800

    init

---
+++ Example/Pods/Target Support Files/HTUILib/HTUILib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HTUILib : NSObject
+@end
+@implementation PodsDummy_HTUILib
+@end
---
+++ Example/Pods/Target Support Files/Pods-HTUILib_Example/Pods-HTUILib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HTUILib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_HTUILib_Example
+@end
